5c07673d559082adc43a90cc04b96d3ed0392d12
Aaron Buck <ambuck@princeton.edu>
2021-09-19 10:11:31 -0400

    helloworld with loop added to repo

---
+++ helloworld.js
@@ -0,0 +1,13 @@
+function add(a,b) {
+    var x = Infinity
+    var m = 4.1
+    while (x > 0) {
+        if (m > 0.0) {
+        console.log("Hello World!");
+        m--;
+        }
+        else break;
+    }
+return a+b
+}
+console.log(add(4,6));
